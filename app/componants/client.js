cae0751b659e92ddeca22cc74cb11c1642aef533
dipak-trio-spire <dipak@trio-spire.com>
2025-02-17 12:10:58 +0530

    ligthhouse changes

--- app/componants/client.js
+++ app/componants/client.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 const ClientCarousel = ({ main_title, section_all_partners }) => {
-  console.log("main_title", main_title);
   const carouselRef = useRef();
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       const loadOwlCarousel = async () => {
@@ -27,15 +27,15 @@ const ClientCarousel = ({ main_title, section_all_partners }) => {
             jQuery(".Client-sliders").owlCarousel({
               loop: true,
               margin: 12,
-              nav: false,
+              nav: true, // Enable navigation
               dots: true,
               items: 6,
               autoplay: true,
               autoplaySpeed: 3000,
               autoplayHoverPause: false,
               navText: [
-                '<img src="/images/Vector(4).png" alt="Previous" />',
-                '<img src="/images/vector5.png" alt="Next" />',
+                '<img src="/images/Vector(4).png" alt="Previous Slide" />',
+                '<img src="/images/vector5.png" alt="Next Slide" />',
               ],
               responsive: {
                 0: { items: 1 },
@@ -45,13 +45,21 @@ const ClientCarousel = ({ main_title, section_all_partners }) => {
                 1350: { items: 6 },
               },
             });
+
+            // Set aria-labels for navigation buttons
             const buttons1 = document.querySelectorAll(".owl-prev");
             const buttons2 = document.querySelectorAll(".owl-next");
-            buttons1.forEach((button, index) => {
-              button.setAttribute("aria-label", `Slide ${index + 1}`);
+            buttons1.forEach((button) => {
+              button.setAttribute("aria-label", "Previous Slide");
             });
-            buttons2.forEach((button, index) => {
-              button.setAttribute("aria-label", `Slide ${index + 1}`);
+            buttons2.forEach((button) => {
+              button.setAttribute("aria-label", "Next Slide");
+            });
+
+            // Set aria-labels for pagination dots
+            const dots = document.querySelectorAll(".owl-dot");
+            dots.forEach((dot, index) => {
+              dot.setAttribute("aria-label", `Go to slide ${index + 1}`);
             });
           };
           document.body.appendChild(owlCarouselJS);
@@ -63,62 +71,59 @@ const ClientCarousel = ({ main_title, section_all_partners }) => {
   }, []);
 
   return (
-    <>
-      <section className="py-[30px] md:py-[40px] lg:py-[50px]">
-        <div className="container px-[15px] mx-auto">
-          <div className="flex flex-col gap-6 md:gap-11 lg:gap-16 text-center">
-            {main_title && <h2>{main_title}</h2>}
-            <div
-              className="owl-carousel Client-sliders relative"
-              ref={carouselRef}
-            >
-              {section_all_partners?.value &&
-                section_all_partners?.value.map((val, index) => {
-                  return (
-                    <div className="item" key={index}>
-                      <div className="client-logo text-center flex items-center justify-center">
-                        {val.partners_section_all_partners_website_link ? (
-                          <Link
-                            href={
-                              val.partners_section_all_partners_website_link
-                                ?.url
-                            }
-                            target={
-                              val.partners_section_all_partners_website_link
-                                ?.target
-                            }
-                            aria-label={
-                              val.partners_section_all_partners_website_link
-                                ?.title
-                            }
-                            className="block"
-                          >
-                            <Image
-                              src={val.partners_section_all_partners_logos}
-                              alt={`client-logo${index + 1}`}
-                              className="!w-auto"
-                              height={85}
-                              width={157}
-                            />
-                          </Link>
-                        ) : (
+    <section className="py-[30px] md:py-[40px] lg:py-[50px]">
+      <div className="container px-[15px] mx-auto">
+        <div className="flex flex-col gap-6 md:gap-11 lg:gap-16 text-center">
+          {main_title && <h2>{main_title}</h2>}
+          <div
+            className="owl-carousel Client-sliders relative"
+            ref={carouselRef}
+          >
+            {section_all_partners?.value &&
+              section_all_partners?.value.map((val, index) => {
+                return (
+                  <div className="item" key={index}>
+                    <div className="client-logo text-center flex items-center justify-center">
+                      {val.partners_section_all_partners_website_link ? (
+                        <Link
+                          href={
+                            val.partners_section_all_partners_website_link?.url
+                          }
+                          target={
+                            val.partners_section_all_partners_website_link
+                              ?.target
+                          }
+                          aria-label={
+                            val.partners_section_all_partners_website_link
+                              ?.title
+                          }
+                          className="block"
+                        >
                           <Image
                             src={val.partners_section_all_partners_logos}
-                            alt={`client-logo${index + 1}`}
+                            alt={`Client logo for ${val.partners_section_all_partners_website_link?.title}`}
                             className="!w-auto"
                             height={85}
                             width={157}
                           />
-                        )}
-                      </div>
+                        </Link>
+                      ) : (
+                        <Image
+                          src={val.partners_section_all_partners_logos}
+                          alt={`Client logo ${index + 1}`}
+                          className="!w-auto"
+                          height={85}
+                          width={157}
+                        />
+                      )}
                     </div>
-                  );
-                })}
-            </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
