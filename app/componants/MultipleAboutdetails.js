a4c670ef31bc9e368af5ff309b6eeafce7208b55
dipak-trio-spire <dipak@trio-spire.com>
2025-03-03 09:37:44 +0530

    new changes

--- app/componants/MultipleAboutdetails.js
+++ app/componants/MultipleAboutdetails.js
@@ -7,20 +7,103 @@ const AsehetikAboutpage = ({ MultipleAboutdeta }) => {
     <>
       {MultipleAboutdeta &&
         MultipleAboutdeta?.value?.map((val, index) => (
-          <>
+          <React.Fragment key={index}>
             {val?.aesthetik_anfrage_1_main_title && (
               <Categories
-                key={index}
                 title={val?.aesthetik_anfrage_1_main_title}
                 description={val?.aesthetik_anfrage_1_content}
                 BTN={val?.aesthetik_anfrage_1_button}
               />
             )}
+
             {val?.aesthetik_all_faltenunterspritzung &&
               val?.aesthetik_all_faltenunterspritzung?.map((valData, ind) => {
+                const isEven = ind % 2 === 0;
                 return (
                   <section
-                    // className="py-[30px] md:py-[40px] lg:py-[50px] bg-Bgslate"
+                    key={ind}
+                    className={`pb-10 py-[30px] md:py-[40px] lg:py-[50px] ${
+                      isEven ? "bg-Bgslate" : "bg-white"
+                    }`}
+                  >
+                    <div className="w-full max-w-[1780px] px-[15px] pe-[15px]">
+                      <div
+                        className={`flex z-10 flex-col lg:flex-row gap-4 md:gap-8 lg:gap-0 py-0 lg:py-[64px] relative 
+                        lg:after:absolute lg:after:top-0 lg:after:border lg:after:border-Teal lg:after:w-[60%] lg:after:h-full lg:after:z-0 ${
+                          isEven
+                            ? "lg:flex-row lg:after:right-0"
+                            : "lg:flex-row-reverse lg:after:left-0"
+                        }`}
+                      >
+                        {/* Image Section */}
+                        <div className="flex justify-center lg:justify-end items-center flex-col w-full lg:w-[55%] h-full">
+                          <Image
+                            src={
+                              valData.aesthetik_all_faltenunterspritzung_image
+                            }
+                            alt="About-img"
+                            objectFit="cover"
+                            className={`relative z-[1] py-0 lg:py-[30px] ${
+                              isEven ? "bg-Bgslate" : "bg-white"
+                            }`}
+                            width={578}
+                            height={578}
+                          />
+                        </div>
+                        <div className="flex gap-5 flex-col w-full justify-center lg:w-[50%] px-0 lg:px-4 lg:gap-8 2xl:px-[100px] relative z-10">
+                          <div className="flex flex-col gap-5 2xl:gap-6 lg:flex-[50%] flex-shrink-0 justify-center">
+                            <div className="flex flex-col gap-4">
+                              <div className="flex relative">
+                                <h2>
+                                  {
+                                    valData.aesthetik_all_faltenunterspritzung_title
+                                  }
+                                </h2>
+                              </div>
+                              <div
+                                className="flex flex-col gap-4 text-body 2xl:text-a [&_ul>li]:font-medium"
+                                dangerouslySetInnerHTML={{
+                                  __html:
+                                    valData?.aesthetik_all_faltenunterspritzung_content,
+                                }}
+                              ></div>
+                              <div
+                                key={ind.id}
+                                className="flex flex-col gap-5 2xl:gap-6 flex-1 lg:flex-[50%] flex-shrink-0"
+                              >
+                                <div className="flex flex-col gap-4 flex-1">
+                                  <h3 className="text-h4 2xl:text-h3">
+                                    {
+                                      ind.home_leistungen_section_sub_content_title
+                                    }
+                                  </h3>
+                                  <div
+                                    className="grid xl:grid-cols-2 gap-4 text-body 2xl:text-a [&_ul>li]:font-medium *:bg-salte *:p-4"
+                                    dangerouslySetInnerHTML={{
+                                      __html:
+                                        valData?.aesthetik_all_faltenunterspritzung_note,
+                                    }}
+                                  ></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                );
+              })}
+          </React.Fragment>
+        ))}
+    </>
+  );
+};
+
+export default AsehetikAboutpage;
+
+{
+  /* <section
                     className={`py-[30px] md:py-[40px] lg:py-[50px] ${
                       ind % 2 === 0 ? "bg-Bgslate" : "bg-white"
                     }`}
@@ -33,7 +116,7 @@ const AsehetikAboutpage = ({ MultipleAboutdeta }) => {
                           ind % 2 === 1 ? "lg:flex-row-reverse" : "lg:flex-row"
                         } gap-4 md:gap-10 2xl:gap-[100px] items-start`}
                       >
-                        {/* Image Section */}
+           
                         <div
                           className={
                             ind % 2 === 1
@@ -54,7 +137,7 @@ const AsehetikAboutpage = ({ MultipleAboutdeta }) => {
                             />
                           </div>
                         </div>
-                        {/* Content Section */}
+                    
                         <div className="flex gap-5 flex-col w-full justify-center lg:w-1/2  2xl:w-[60%] ps-0 lg:px-4 lg:gap-[25px]">
                           <div className="flex relative">
                             <h2>
@@ -78,13 +161,5 @@ const AsehetikAboutpage = ({ MultipleAboutdeta }) => {
                         </div>
                       </div>
                     </div>
-                  </section>
-                );
-              })}
-          </>
-        ))}
-    </>
-  );
-};
-
-export default AsehetikAboutpage;
+                  </section> */
+}
