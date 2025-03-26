6f6edfd899b6998d6e1c97168edda4c5fea1c1a0
dipak-trio-spire <dipak@trio-spire.com>
2025-03-26 15:13:58 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,68 @@ const nextConfig = {
     domains: ["daniella.blog-s.de"], // Add Instagram CDN here
     formats: ["image/webp"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/heilpraktikerin-fusgoenheim",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/homoopathie-ludwigshafen",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/homoeopathie-mannheim", destination: "/", permanent: true },
+      { source: "/homoeopathie-worms", destination: "/", permanent: true },
+      { source: "/schmerztherapie-worms", destination: "/", permanent: true },
+      {
+        source: "/schmerztherapie-mannheim",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/schmerztherapie-ludwigshafen",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/schmerztherapie-heidelberg",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/schmerztherapie-frankenthal",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/schmerztherapie-bad-duerkheim",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/plasma-pen-behandlung-mannheim",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/plasma-pen-behandlung-ludwigshafen",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/plasma-pen-behandlung-kosten",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/plasma-pen-behandlung-frankenthal",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
