383a914d038ef030827e1bacea8caa8541fe7ff5
dipak-trio-spire <dipak@trio-spire.com>
2025-03-31 18:21:40 +0530

    new changes

--- app/componants/Banner.js
+++ app/componants/Banner.js
@@ -10,7 +10,7 @@ const BannerCarousel = ({ title, img, content, BTN }) => {
           <div className="item">
             <div className="bg-banner bg-banner-img bg-cover px-[15px] 2xl:ps-[148px]">
               <Image
-                src={img} // Keep this as the main source, fallback if srcSet fails
+                src={img} 
                 className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
                 alt="hero banner image"
                 role="img"
@@ -18,11 +18,6 @@ const BannerCarousel = ({ title, img, content, BTN }) => {
                 height={850}
                 priority={true}
                 sizes="(max-width: 640px) 100vw, (max-width: 1024px) 100vw, 1920px"
-                srcSet="
-                  https://daniella.blog-s.de/wp-content/uploads/2025/03/Start-Banner.webp?w=640&h=280&fit=cover 640w,
-                  https://daniella.blog-s.de/wp-content/uploads/2025/03/Start-Banner.webp?w=1024&h=440&fit=cover 1024w,
-                  https://daniella.blog-s.de/wp-content/uploads/2025/03/Start-Banner.webp?w=1920&h=850&fit=cover 1920w
-                "
               />
               <div className="flex flex-col bg-Bgwhite p-6 lg:p-12 gap-4 lg:gap-8 w-full md:w-[845px]">
                 <h1
