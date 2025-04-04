50f08cf4a1101718370347dfad53c858a54f79ea
dipak-trio-spire <dipak@trio-spire.com>
2025-04-04 09:53:26 +0530

    new changes

--- app/componants/Loader.js
+++ app/componants/Loader.js
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.3)] z-50">
       <div className="w-16 h-16 border-4 border-Teal border-t-transparent rounded-full animate-spin "></div>
     </div>
   );
