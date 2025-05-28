09750de746e89e3a38b46e244b75e04ec67616c5
dipak-trio-spire <dipak@trio-spire.com>
2025-05-28 18:46:08 +0530

    add sitemap

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,40 +1,67 @@
 // app/sitemap.xml/route.js
-import FetchAllslug from "../until/FetchAllslug";
 
 export async function GET() {
-  const siteUrl =
-    process.env.NEXT_DOMIN_URL || "https://daniella-nicolli-nextjs.vercel.app";
+  const siteUrl = process.env.NEXT_DOMIN_URL || "https://daniella-nicolli-nextjs.vercel.app"; // Replace with your actual domain
 
-  try {
-    const Data = await FetchAllslug("/custom-sitemap/v1/slugs");
+  const SiteMapData = [
+    "fett-weg-spritze-mannheim",
+    "fett-weg-spritze-ludwigshafen",
+    "fett-weg-spritze-heidelberg",
+    "fett-weg-spritze-frankenthal",
+    "microneedling-mannheim",
+    "microneedling-ludwigshafen",
+    "microneedling-gruenstadt",
+    "microneedling-frankenthal",
+    "heilpraktikerin-worms",
+    "heilpraktikerin-ludwigshafen",
+    "heilpraktikerin-freinsheim",
+    "heilpraktikerin-bad-duerkheim",
+    "heilpraktiker-maxdorf",
+    "heilpraktiker-mannheim",
+    "heilpraktiker-gruenstadt",
+    "heilpraktiker-frankenthal",
+    "faltenunterspritzung-hyaluronsaeure",
+    "faltenunterspritzung-mannheim",
+    "faltenunterspritzung-ludwigshafen",
+    "faltenunterspritzung-lambsheim",
+    "faltenunterspritzung-gruenstadt",
+    "faltenbehandlung-mannheim",
+    "faltenbehandlung-heidelberg",
+    "fadenlifting-mannheim",
+    "faltenbehandlung-frankenthal",
+    "fadenlifting-ludwigshafen",
+    "landing-page",
+    "datenschutzerklarung",
+    "impressum",
+    "blog",
+    "ratgerber",
+    "ueber-mich",
+    "naturheilmedizin",
+    "kontakt",
+    "aesthetik",
+    "home",
+  ];
 
-    if (!Array.isArray(Data)) {
-      throw new Error("Invalid response format");
-    }
+  // Remove duplicates
+  const uniquePaths = [...new Set(SiteMapData)];
 
-    const uniquePaths = [...new Set(Data)];
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${uniquePaths
+      .map(
+        (slug) => `
+    <url>
+      <loc>${siteUrl}/${slug}</loc>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>`
+      )
+      .join("")}
+  </urlset>`;
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${uniquePaths
-    .map(
-      (slug) => `
-  <url>
-    <loc>${siteUrl}/${slug.replace(/^\/+/, "")}</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>`
-    )
-    .join("")}
-</urlset>`;
-
-    return new Response(sitemap, {
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    });
-  } catch (error) {
-    console.error("Error generating sitemap:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
