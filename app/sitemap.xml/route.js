42a564eb4c44e51e46fa3af0eb637ab1ab7c1127
dipak-trio-spire <dipak@trio-spire.com>
2025-05-29 09:55:59 +0530

    add sitemap

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,48 +1,15 @@
 // app/sitemap.xml/route.js
+import FetchAllslug from "../until/FetchAllslug"
 
 export async function GET() {
-  const siteUrl = process.env.NEXT_DOMIN_URL || "https://daniella-nicolli-nextjs.vercel.app"; // Replace with your actual domain
-
-  const SiteMapData = [
-    "fett-weg-spritze-mannheim",
-    "fett-weg-spritze-ludwigshafen",
-    "fett-weg-spritze-heidelberg",
-    "fett-weg-spritze-frankenthal",
-    "microneedling-mannheim",
-    "microneedling-ludwigshafen",
-    "microneedling-gruenstadt",
-    "microneedling-frankenthal",
-    "heilpraktikerin-worms",
-    "heilpraktikerin-ludwigshafen",
-    "heilpraktikerin-freinsheim",
-    "heilpraktikerin-bad-duerkheim",
-    "heilpraktiker-maxdorf",
-    "heilpraktiker-mannheim",
-    "heilpraktiker-gruenstadt",
-    "heilpraktiker-frankenthal",
-    "faltenunterspritzung-hyaluronsaeure",
-    "faltenunterspritzung-mannheim",
-    "faltenunterspritzung-ludwigshafen",
-    "faltenunterspritzung-lambsheim",
-    "faltenunterspritzung-gruenstadt",
-    "faltenbehandlung-mannheim",
-    "faltenbehandlung-heidelberg",
-    "fadenlifting-mannheim",
-    "faltenbehandlung-frankenthal",
-    "fadenlifting-ludwigshafen",
-    "landing-page",
-    "datenschutzerklarung",
-    "impressum",
-    "blog",
-    "ratgerber",
-    "ueber-mich",
-    "naturheilmedizin",
-    "kontakt",
-    "aesthetik",
-    "home",
-  ];
-
-  // Remove duplicates
+  const siteUrl = process.env.NEXT_DOMIN_URL || "https://daniella-nicolli-nextjs.vercel.app"; 
+  let SiteMapData;
+  try {
+     SiteMapData = await FetchAllslug("/custom-sitemap/v1/slugs");
+    console.log('SiteMapData', SiteMapData)
+  } catch (error) {
+    console.log('error', error)
+  }
   const uniquePaths = [...new Set(SiteMapData)];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -52,8 +19,6 @@ export async function GET() {
         (slug) => `
     <url>
       <loc>${siteUrl}/${slug}</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
     </url>`
       )
       .join("")}
