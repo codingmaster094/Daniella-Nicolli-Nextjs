7bcc8e96fe7ebc20b83d04a26a507acb45b7ad19
dipak-trio-spire <dipak@trio-spire.com>
2025-06-18 12:27:43 +0530

    add no-profile-image

--- app/layout.js
+++ app/layout.js
@@ -7,29 +7,33 @@ import CanonicalTag from "./componants/CanonicalTag";
 import HeaderDatas from "./until/HeaderData";
 
 export default async function RootLayout({ children }) {
-let Schema_markup
-    try {
-      Schema_markup = await HeaderDatas("/acf-options");
-    } catch (error) {
-      console.error("Error fetching header data:", error);
-    }
+  let Schema_markup = null;
+  try {
+    Schema_markup = await HeaderDatas("/acf-options");
+  } catch (error) {
+    console.error("Error fetching header data:", error);
+  }
 
-  const schemaData = {
-    "@context": Schema_markup.context,
-    "@type": Schema_markup.context_type,
-    name: Schema_markup.business_name,
-    image: Schema_markup.business_image.url,
-    telephone: Schema_markup.business_phone,
-    address: {
-      "@type": Schema_markup.bussiness_address.address_type,
-      streetAddress: Schema_markup.bussiness_address.street_address,
-      addressLocality: Schema_markup.bussiness_address.city,
-      postalCode: Schema_markup.bussiness_address.postal_code,
-      addressCountry: Schema_markup.bussiness_address.country_code,
-    },
-    url: Schema_markup.business_url.url,
-  };
-
+  // Fallback or safe handling if Schema_markup is null
+  const schemaData = Schema_markup
+    ? {
+        "@context": Schema_markup.context,
+        "@type": Schema_markup.context_type,
+        name: Schema_markup.business_name,
+        image: Schema_markup.business_image?.url,
+        telephone: Schema_markup.business_phone,
+        address: {
+          "@type": Schema_markup.bussiness_address?.address_type,
+          streetAddress: Schema_markup.bussiness_address?.street_address,
+          addressLocality: Schema_markup.bussiness_address?.city,
+          postalCode: Schema_markup.bussiness_address?.postal_code,
+          addressCountry: Schema_markup.bussiness_address?.country_code,
+        },
+        url: Schema_markup.business_url?.url,
+      }
+    : null;
+    
+  console.log('schemaData', schemaData)
   return (
     <html lang="en">
       <head>
