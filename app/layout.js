95e897101af2d564cbb37f4e32d9f656feabbce8
dipak-trio-spire <dipak@trio-spire.com>
2025-06-19 14:05:23 +0530

    add Rank math SEO Schema Markup

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,12 @@ import "../public/css/stylesheet.css";
 import Footer from "./Footer/page";
 import TopButton from "../app/componants/Top-Button";
 import Header from "./Header/page";
-import CanonicalTag from "./componants/CanonicalTag";
+
 
 export default async function RootLayout({ children }) {  
   return (
     <html lang="en">
       <body>
-        {/* <CanonicalTag /> */}
         <Header />
         <main>{children}</main>
         <TopButton />
