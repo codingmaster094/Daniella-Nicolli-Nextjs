330b949b8a738cf118205b139b0dbc9e8bc35569
dipak-trio-spire <dipak@trio-spire.com>
2025-06-19 15:44:30 +0530

    change SEO home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
 import HomePage from "./Home/page";
 import MetaDataAPIS from "./until/metadataAPI";
-
+import dynamic from "next/dynamic";
+const SchemaInjector = dynamic(() => import("./componants/SchemaInjector"));
 export default async function Home() {
-  return <HomePage />;
+  let schemaJSON;
+  try {
+    const metadata = await MetaDataAPIS("/aesthetik");
+    const schemaMatch = metadata.head.match(
+      /<script[^>]*type="application\/ld\+json"[^>]*class="rank-math-schema"[^>]*>([\s\S]*?)<\/script>/
+    );
+    schemaJSON = schemaMatch ? schemaMatch[1].trim() : null;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error loading data.</div>;
+  }
+  return (
+    <>
+      <SchemaInjector schemaJSON={schemaJSON} />
+      <HomePage />
+    </>
+  );
 }
 export async function generateMetadata() {
   let metadata = await MetaDataAPIS("/home");
@@ -12,13 +29,20 @@ export async function generateMetadata() {
   const descriptionMatch = metadata.head.match(
     /<meta name="description" content="(.*?)"/
   );
-
+  const canonicalMatch = metadata.head.match(
+    /<link\s+rel="canonical"\s+href="([^"]+)"/i
+  );
   const title = titleMatch ? titleMatch[1] : "Default Title";
   const description = descriptionMatch
     ? descriptionMatch[1]
     : "Default Description";
+    const canonical =
+      canonicalMatch?.[1] || "https://daniella-nicolli-nextjs.vercel.app";
   return {
     title,
     description,
+    alternates: {
+      canonical,
+    },
   };
 }
